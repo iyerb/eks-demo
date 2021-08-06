d9794b54318cdafb665c9c75d6d50422a556c9d6
iyerb <iyerb@amazon.com>
2021-08-06 17:32:47 +0800

    Fixed service names

---
+++ lib/cdk-eks-app-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+import * as cdk8s from 'cdk8s';
+import { MyChart } from './my-chart';
+
+export class CdkEksAppStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /**
+     * Code to create the EKS Cluster
+     */
+    const cluster = new eks.Cluster(this, 'ckdekscluster', {
+      version: eks.KubernetesVersion.V1_18,
+    });
+
+    /**
+     * Code to add pods on eks cluster. All containers are define in MyChart
+     */
+    cluster.addCdk8sChart('my-chart', new MyChart(new cdk8s.App(), 'MyChart'));
+  }
+}
